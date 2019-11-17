a82b993cbb6d536564026512ff29b4520008740b
Kirn Gill II <segin2005@gmail.com>
2019-11-17 17:17:51 -0600

    Added minimum/maximum initial allocation sizes

--- main.c
+++ main.c
@@ -30,6 +30,8 @@ int main(int argc, char *argv[]) {
                     (((mz->pageCount - 1) * 512) + mz->lastPageSize));
                 printf("Total relocation entries:\t0x%04x\n", mz->relocationEntries);
                 printf("Header size in paragraphs:\t0x%04x (%d bytes)\n", mz->hdrSize, (mz->hdrSize * 16));
+                printf("Minimum memory in paragraphs:\t0x%04x (%d bytes)\n", mz->minMemory, (mz->minMemory * 16));
+                printf("Maximum memory in paragraphs:\t0x%04x (%d bytes)\n", mz->maxMemory, (mz->maxMemory * 16));
                 /* XXX: Do something */
                 if(mz->relocationOffset >= 0x40) {
                     if (!(mzx = malloc(sizeof(struct exe_mz_new_header)))) err(1, "Cannot allocate memory");
