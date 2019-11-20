16e0364c2543b11a9cf12f8fe4e38024cb72f2d0
Kirn Gill II <segin2005@gmail.com>
2019-11-19 23:23:24 -0600

    Added copyright text, fall back to err() from libbsd

--- main.c
+++ main.c
@@ -1,7 +1,22 @@
+/* readexe - Prints EXE info a la objdump/dumpbin/efd 
+ * Copyright © 2019 Kirn Gill II <segin2005@gmail.com>
+ * Permission to use, copy, modify, and/or distribute this software for
+ * any purpose with or without fee is hereby granted, provided that the
+ * above copyright notice and this permission notice appear in all copies.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
+ * WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES 
+ * OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE 
+ * FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY 
+ * DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER 
+ * IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING 
+ * OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
 #include <stdint.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <err.h>
+#include "err.h" /* Our local copy is a compatibility fallback, use libc's first */
 
 /* Big assumptions on little-endianiness here */
 
@@ -175,6 +190,10 @@ int main(int argc, char *argv[]) {
     struct exe_mz_new_header *mzx = NULL;
     FILE *fd;
     int ret;
+
+#ifdef NEED_ERR
+    __progname = argv[0];
+#endif
 
     if (argc < 2) errx(1, "Not enough arguments.");
     if (!(fd = fopen(argv[1], "rb"))) err(1, "Cannot open %s", argv[1]);
