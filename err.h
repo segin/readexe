fcf3ba1c4588e94f99cf37e735ca077e9927f864
Kirn Gill II <segin2005@gmail.com>
2019-11-19 23:45:07 -0600

    Added associated header for err.c

---
+++ err.h
@@ -0,0 +1,17 @@
+#include <stdarg.h>
+#include <stdint.h>
+
+#define NEED_ERR
+
+extern char *__progname;
+
+void vwarnc(int code, const char *format, va_list ap);
+void verrc(int status, int code, const char *format, va_list ap);
+void warn(const char *format, ...);
+void vwarnx(const char *format, va_list ap);
+void warnx(const char *format, ...);
+void err(int status, const char *format, ...);
+void verrx(int eval, const char *format, va_list ap);
+void errx(int eval, const char *format, ...);
+void setprogname(const char *progname);
+const char *getprogname();
