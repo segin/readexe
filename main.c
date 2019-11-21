7a2f57f8cad4949b81e1b8cf0982d1eec95717db
Kirn Gill II <segin2005@gmail.com>
2019-11-21 17:22:16 -0600

    Various fixes for C type size differences on 16-bit platforms

--- main.c
+++ main.c
@@ -74,11 +74,11 @@ void read_ne_segments(FILE *fd, const struct exe_ne_header *ne, const char fname
                     neseg[i].discardable ? "DISCARD " : ""
                 );
                 printf("  Offset      (file)   Length   (dec) \n");
-                printf("  0x%04x  0x%08x   0x%04x   %5d\n\n", 
+                printf("  0x%04x  0x%08lx   0x%04x   %5lu\n\n", 
                     neseg[i].segmentOffset, 
-                    neseg[i].segmentOffset << ne->offsetShiftCount, 
-                    neseg[i].segmentSize ? neseg[i].segmentSize : 0x10000, 
-                    neseg[i].segmentSize ? neseg[i].segmentSize : 0x10000);
+                    ((unsigned long int) neseg[i].segmentOffset << ne->offsetShiftCount), 
+                    neseg[i].segmentSize ? neseg[i].segmentSize : 0x10000UL, 
+                    neseg[i].segmentSize ? neseg[i].segmentSize : 0x10000UL);
 
             }
         }
@@ -91,11 +91,11 @@ void read_ne_header(const struct exe_ne_header *ne, const struct exe_mz_new_head
     char *msg;
     printf("Debug: sizeof(struct exe_ne_header): %lu\n", sizeof(struct exe_ne_header));
     printf("New Executable with magic:\t%c%c\n", ne->magic[0], ne->magic[1]);
-    printf("Linker version:\t\t\t%d.%d\n", ne->linkerMajor, ne->linkerMinor);
-    printf("Entry table offset:\t\t0x%04x (File offset 0x%08x)\n", ne->entryTableOffset, (ne->entryTableOffset + mzx->nextHeader));
-    printf("Entry table size:\t\t0x%04x (%d bytes)\n", ne->entryTableSize, ne->entryTableSize);
-    printf("Header CRC:\t\t\t0x%08x\n", ne->fileCrc);
-    printf(".EXE Flags:\t\t\t0x%02x\n", ne->progFlags);
+    printf("Linker version:\t\t\t%hhu.%hhu\n", ne->linkerMajor, ne->linkerMinor);
+    printf("Entry table offset:\t\t0x%04x (File offset 0x%08lx)\n", ne->entryTableOffset, (unsigned long int) (ne->entryTableOffset + mzx->nextHeader));
+    printf("Entry table size:\t\t0x%04x (%hu bytes)\n", ne->entryTableSize, ne->entryTableSize);
+    printf("Header CRC:\t\t\t0x%08lx\n", ne->fileCrc);
+    printf(".EXE Flags:\t\t\t0x%02hhx\n", ne->progFlags);
     switch(ne->dataType) {
         case DATA_NONE:
             msg = "Not indicated";
@@ -117,7 +117,7 @@ void read_ne_header(const struct exe_ne_header *ne, const struct exe_mz_new_head
     printf(" - 80286 opcodes used:\t\t%s\n", ne->ops80286 ? "true" : "false");
     printf(" - 80386 opcodes used:\t\t%s\n", ne->ops80386 ? "true" : "false");
     printf(" - FPU/80x87 opcodes used:\t%s\n", ne->ops80x87 ? "true" : "false");
-    printf("Application flags:\t\t0x%02x\n", ne->appFlags);
+    printf("Application flags:\t\t0x%02hhx\n", ne->appFlags);
     switch(ne->appType) {
         case APP_NONE:
             msg = "Not indicated";
@@ -142,17 +142,17 @@ void read_ne_header(const struct exe_ne_header *ne, const struct exe_mz_new_head
     printf("Initial stack size:\t\t0x%04x\n", ne->initStackSize);
     printf("Initial CS:IP (entrypoint):\t%04x:%04x\n", (ne->entryPoint >> 16), (ne->entryPoint & 0xFFFF));
     printf("Initial SS:SP (stack):\t\t%04x:%04x\n", (ne->initStackPtr >> 16), (ne->initStackPtr & 0xFFFF));
-    printf("Segment count:\t\t\t0x%04x (%d)\n", ne->segmentCount, ne->segmentCount);
-    printf("Module reference count:\t\t%04x (%d)\n", ne->modRefCount, ne->modRefCount);
-    printf("Non-resident name table size:\t0x%04x (%d bytes)\n", ne->nonResidentTableSize, ne->nonResidentTableSize);
-    printf("Offset of segment table:\t0x%04x (File offset 0x%08x)\n", ne->segmentTableOffset, (ne->segmentTableOffset << ne->offsetShiftCount));
-    printf("Offset of resource table:\t0x%04x (File offset 0x%08x)\n", ne->resourceTableOffset, (ne->resourceTableOffset << ne->offsetShiftCount));
-    printf("Offset of resident name table:\t0x%04x (File offset 0x%08x)\n", ne->residentNamesTableOffset, (ne->residentNamesTableOffset << ne->offsetShiftCount));
-    printf("Offset of module table:\t\t0x%04x (File offset 0x%08x)\n", ne->modulesTableOffset, (ne->modulesTableOffset << ne->offsetShiftCount));
-    printf("Offset of imported names table:\t0x%04x (File offset 0x%08x)\n", ne->importedNamesTableOffset, (ne->importedNamesTableOffset << ne->offsetShiftCount));
-    printf("Non-resident names table:\t0x%08x (File offset)\n", ne->nonResidentTableOffset);
-    printf("Movable entry points:\t\t0x%08x (%d)\n", ne->movableEntryPoints, ne->movableEntryPoints);
-    printf("Windows version:\t\t%d.%d (0x%04x)\n", ne->windowsVersionMajor, ne->windowsVersionMinor, ne->windowsVersion); 
+    printf("Segment count:\t\t\t0x%04x (%u)\n", ne->segmentCount, ne->segmentCount);
+    printf("Module reference count:\t\t%04x (%u)\n", ne->modRefCount, ne->modRefCount);
+    printf("Non-resident name table size:\t0x%04x (%u bytes)\n", ne->nonResidentTableSize, ne->nonResidentTableSize);
+    printf("Offset of segment table:\t0x%04x (File offset 0x%08lx)\n", ne->segmentTableOffset, (ne->segmentTableOffset << ne->offsetShiftCount));
+    printf("Offset of resource table:\t0x%04x (File offset 0x%08lx)\n", ne->resourceTableOffset, (ne->resourceTableOffset << ne->offsetShiftCount));
+    printf("Offset of resident name table:\t0x%04x (File offset 0x%08lx)\n", ne->residentNamesTableOffset, (ne->residentNamesTableOffset << ne->offsetShiftCount));
+    printf("Offset of module table:\t\t0x%04x (File offset 0x%08lx)\n", ne->modulesTableOffset, (ne->modulesTableOffset << ne->offsetShiftCount));
+    printf("Offset of imported names table:\t0x%04x (File offset 0x%08lx)\n", ne->importedNamesTableOffset, (ne->importedNamesTableOffset << ne->offsetShiftCount));
+    printf("Non-resident names table:\t0x%08lx (File offset)\n", ne->nonResidentTableOffset);
+    printf("Movable entry points:\t\t0x%08lx (%lu)\n", ne->movableEntryPoints, ne->movableEntryPoints);
+    printf("Windows version:\t\t%hhu.%hhu (0x%04x)\n", ne->windowsVersionMajor, ne->windowsVersionMinor, ne->windowsVersion); 
 }
 
 void read_next_header(FILE *fd, const struct exe_mz_new_header *mzx, const char fname[]) {
@@ -170,16 +170,16 @@ void read_next_header(FILE *fd, const struct exe_mz_new_header *mzx, const char 
         } else {
             if (((next_magic[0] == 'N') && (next_magic[1] == 'E'))) {
                 printf("\n\n");
-                printf("New Executable header found at offset 0x%08x\n", mzx->nextHeader);
+                printf("New Executable header found at offset 0x%08lx\n", mzx->nextHeader);
                 read_ne_exe(fd, mzx, fname);
             } else if (((next_magic[0] == 'P') && (next_magic[1] == 'E'))) {
                 printf("\n\n");
-                printf("Portable Executable header found at offset 0x%08x\n", mzx->nextHeader);
+                printf("Portable Executable header found at offset 0x%08lx\n", mzx->nextHeader);
                 // read_pe_exe(fd, mzx, fname);
             } else if (((next_magic[0] == 'L') && (next_magic[1] == 'E')) ||
                        ((next_magic[0] == 'L') && (next_magic[1] == 'X'))) {
                 printf("\n\n");
-                printf("Linear Executable header found at offset 0x%08x\n", mzx->nextHeader);
+                printf("Linear Executable header found at offset 0x%08lx\n", mzx->nextHeader);
                 // read_le_exe(fd, mzx, fname);
             } else {
                 printf("\n\n");
@@ -211,15 +211,15 @@ int main(int argc, char *argv[]) {
             ||  ((mz->magic[1] == 'M') && (mz->magic[0] == 'Z')) ) {
                 printf("%s:\n", argv[1]);
                 printf("DOS executable with magic:\t%c%c\n", mz->magic[0], mz->magic[1]);
-                printf("Number of executable pages:\t0x%04x (%d+ bytes)\n", mz->pageCount, ((mz->pageCount - 1) * 512));
-                printf("Size of final page:\t\t%d bytes\n", mz->lastPageSize);
-                printf("Total code size:\t\t0x%08x (%d bytes)\n", 
-                    (((mz->pageCount - 1) * 512) + mz->lastPageSize),
-                    (((mz->pageCount - 1) * 512) + mz->lastPageSize));
+                printf("Number of executable pages:\t0x%04x (%lu+ bytes)\n", mz->pageCount, (unsigned long int)((mz->pageCount - 1UL) * 512UL));
+                printf("Size of final page:\t\t%u bytes\n", mz->lastPageSize);
+                printf("Total code size:\t\t0x%08lx (%lu bytes)\n",
+                    (unsigned long int)(((mz->pageCount - 1) * 512UL) + mz->lastPageSize),
+                    (unsigned long int)(((mz->pageCount - 1) * 512UL) + mz->lastPageSize));
                 printf("Total relocation entries:\t0x%04x\n", mz->relocationEntries);
-                printf("Header size in paragraphs:\t0x%04x (%d bytes)\n", mz->hdrSize, (mz->hdrSize * 16));
-                printf("Minimum memory in paragraphs:\t0x%04x (%d bytes)\n", mz->minMemory, (mz->minMemory * 16));
-                printf("Maximum memory in paragraphs:\t0x%04x (%d bytes)\n", mz->maxMemory, (mz->maxMemory * 16));
+                printf("Header size in paragraphs:\t0x%04x (%lu bytes)\n", mz->hdrSize, (unsigned long int)(mz->hdrSize * 16UL));
+                printf("Minimum memory in paragraphs:\t0x%04x (%lu bytes)\n", mz->minMemory, (unsigned long int)(mz->minMemory * 16UL));
+                printf("Maximum memory in paragraphs:\t0x%04x (%lu bytes)\n", mz->maxMemory, (unsigned long int)(mz->maxMemory * 16UL));
                 printf("Initial stack segment:\t\t0x%04x\n", mz->stackSegment);
                 printf("Initial stack pointer:\t\t0x%04x\n", mz->stackPointer);
                 printf("Checksum:\t\t\t0x%04x\n", mz->checksum);
@@ -234,7 +234,7 @@ int main(int argc, char *argv[]) {
                         if ((ret = ferror(fd))) warn("Cannot read %s", argv[1]);
                         if ((ret = feof(fd))) warnx("Unexpected end of file: %s", argv[1]);
                     } else {
-                        printf("Offset to NE/PE header:\t\t0x%08x\n", mzx->nextHeader);
+                        printf("Offset to NE/PE header:\t\t0x%08lx\n", mzx->nextHeader);
                         read_next_header(fd, mzx, argv[1]);
                     }
                 }
